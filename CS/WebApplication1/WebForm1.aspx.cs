d56f043620e34d3f79410a48ada31a26d34b6f80
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-26 00:22:10 +0400

    Change example status to out of maintenance

--- CS/WebApplication1/WebForm1.aspx.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace WebApplication1 {
-    public partial class WebForm1 : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
-            ASPxReportDesigner1.OpenReport(new XtraReport1());
-        }
-    }
-}
